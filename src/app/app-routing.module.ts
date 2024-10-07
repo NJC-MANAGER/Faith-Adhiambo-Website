9f11dbd9feeb150805cc9ba5e7d90f4d7b2fa6f1
FaithAdhiambo456 <156072384+FaithAdhiambo456@users.noreply.github.com>
2024-10-07 08:11:09 +0300

    lazy loading the key modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './dashboard/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/dashboard/home', pathMatch: 'full'},
+    {
+      path: "authentication",
+     // component: LoginComponent,
+      loadChildren: () =>
+        import("./authentication/authentication.module").then((m) => m.AuthenticationModule),
+    },
+    {
+      path: "dashboard",
+      component: HomeComponent,
+      loadChildren: () =>
+        import("./dashboard/dashboard.module").then((m) => m.DashboardModule)
+    },
+    {path: "events",
+      loadChildren: () => 
+        import("./events/events.module").then((m) => m.EventsModule)
+    },
+    {path: "services",
+      loadChildren: () =>
+        import("./services/services.module").then((m) => m.ServicesModule)
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
